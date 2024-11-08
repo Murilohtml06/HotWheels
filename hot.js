b3001ae8774ab66af9cb684984c5be1f462b8ca5
Murilo Paulo <161150363+Murilohtml06@users.noreply.github.com>
2024-11-07 21:24:21 -0300

    atualização 1.9

--- hot.js
+++ hot.js
@@ -9,3 +9,21 @@ function menuShow() {
 
     }
 }
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+    const carousel = document.querySelector('.carousel-content');
+    const items = document.querySelectorAll('.carousel-item');
+    const totalItems = items.length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalItems - 1;
+    } else if (currentIndex >= totalItems) {
+        currentIndex = 0;
+    }
+
+    const offset = -currentIndex * 100;
+    carousel.style.transform = `translateX(${offset}%)`;
+}
